74d0de4c3b9843b608010e2f10b21463dbb6acdc
Godzilla <parks37370@gmail.com>
2022-07-12 07:21:05 -0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import "./Navbarstyles.css";
+import React from "react";
+import {Link} from "react-router-dom";
+import {FaBars} from "react-icons/fa"
+
+
+
+const Navbar =() =>{
+  return (
+    <div className="header">
+     <Link to="/">
+     <h1>MIL<span className="menu-name">TECH.</span></h1>
+      </Link>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+
+      </ul>
+      <div className="hamburger">
+        <FaBars size={20} style={{ color: "#fff"}}/>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
